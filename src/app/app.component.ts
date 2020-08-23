d90aff5f4e494d2ffda973e8db6abfa68eaf0375
Robertgaraban <robertgaraban@gmail.com>
2020-08-22 22:07:22 -0300

    g

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+import { UsuarioService } from './usuario.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   title = 'formulario';
+
+  usuario: any = {};
+
+  constructor(private usuServicio: UsuarioService) {
+
+    usuServicio.getUsuario().subscribe(usuario => this.usuario = this.usuario)
+
+  }
+
+  onSubmit(formulario){
+    console.log(formulario.form.value);
+  }
+
 }
